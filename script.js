90319652abd7421b27c1cecbc7d37139e32c707d
sarah181208 <00001095456246sp@aluno.educacao.sp.gov.br>
2025-08-29 15:58:07 -0300

    Update script.js

--- script.js
+++ script.js
@@ -16,5 +16,14 @@ diminuiFonteBotao.addEventListener('click', function () {
   document.body.style.fontSize = \${tamanhoAtualFonte}rem``
 
   })
-
+const botaoDeAcessibilidade =
+document.getElementById('botaoacessibilidade');
+const opcoesDeAcessibilidade =
+document.getElementById('opcoesacessibilidade');
+ botaoDeAcessibilidade.addEventListener('click', () => {
+ botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+ opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+ })
+   alternaContraste.addEventListener('click', function(){
+    document.body.classList.toggle('alto-contraste');
    })                        
