27cc5b5ed98f56499109ba54b9c0d59b68390875
sarah181208 <00001095456246sp@aluno.educacao.sp.gov.br>
2025-09-05 15:40:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -30,3 +30,7 @@ document.getElementById('opcoesacessibilidade');
    alternaContraste.addEventListener('click', function(){
     document.body.classList.toggle('alto-contraste');
    })                        
+ScrollReveal().reveal('#inicio', { delay: 500 });
+ScrollReveal().reveal('#tropicalia', { delay: 500 });
+ ScrollReveal().reveal('#galeria', { delay: 500 });
+ScrollReveal().reveal('#contato', { delay: 500 });
